66054d582b77efc28ca23f682406ad3d96efe1d9
Mike Doyle <mtdoyle@gmail.com>
2018-10-30 05:35:05 -0500

    added WILL DO! card and made the sleeps a little smarter

--- src/virginpulsedailies.py
+++ src/virginpulsedailies.py
@@ -10,7 +10,8 @@ def virginpulsedailies():
     chromedriver_path = os.getenv("CHROMEDRIVERPATH")
     driver = webdriver.Chrome(chromedriver_path)
     driver.get('http://member.virginpulse.com/login.aspx')
-    time.sleep(10)
+    while not driver.find_elements_by_id('username'):
+        time.sleep(1)
     username_box = driver.find_element_by_id('username')
     username_box.click()
     username_box.send_keys(username)
@@ -30,7 +31,11 @@ def virginpulsedailies():
         time.sleep(2)
 
     driver.find_element_by_id('kc-login').click()
-    time.sleep(20)
+    while not driver.find_elements_by_id('basic-header'):
+        # sleep until page loads
+        time.sleep(1)
+    # an extra 2 seconds of sleep until page is fully loaded
+    time.sleep(2)
     trophy_box = driver.find_elements_by_id('trophy-modal-close-btn')
     if trophy_box:
         trophy_box[0].click()
@@ -38,10 +43,10 @@ def virginpulsedailies():
     daily_tips_done = driver.find_elements_by_class_name('daily-tips-all-done')
     cycle = 0
     while cycle < 5 and not daily_tips_done:
-        got_it_card = driver.find_elements_by_id('triggerCloseCurtain')
-        if got_it_card:
-            if got_it_card[0].text == 'GOT IT!':
-                got_it_card[0].click()
+        daily_tip_card = driver.find_elements_by_id('triggerCloseCurtain')
+        if daily_tip_card:
+            if daily_tip_card[0].text == 'GOT IT!' or daily_tip_card[0].text == 'WILL DO!':
+                daily_tip_card[0].click()
         true_false_card = driver.find_elements_by_class_name('quiz-true-false-buttons')
         if true_false_card:
             true_false_card[0].click()
@@ -56,8 +61,9 @@ def virginpulsedailies():
     driver.execute_script('document.querySelector(\'#page-wrapper > div > div > div > basic-home > div > div > div:nth-child(4) > home-healthy-habits > div > div > div:nth-child(1) > home-healthy-habit-tile > div > div.home-healthy-habit-yesno.ng-scope > button.btn-primary-inverse.vp-button-primary-inverse.yesNo-btn.yes-btn.ng-scope\').click()')
     driver.execute_script('document.querySelector(\'#page-wrapper > div > div > div > basic-home > div > div > div:nth-child(4) > home-healthy-habits > div > div > div:nth-child(2) > home-healthy-habit-tile > div > div.home-healthy-habit-yesno.ng-scope > button.btn-primary-inverse.vp-button-primary-inverse.yesNo-btn.yes-btn.ng-scope\').click()')
     driver.execute_script('document.querySelector(\'#page-wrapper > div > div > div > basic-home > div > div > div:nth-child(4) > home-healthy-habits > div > div > div:nth-child(3) > home-healthy-habit-tile > div > div.home-healthy-habit-yesno.ng-scope > button.btn-primary-inverse.vp-button-primary-inverse.yesNo-btn.yes-btn.ng-scope\').click()')
-    driver.execute_script('document.querySelector(\'#page-wrapper > div > div > div > basic-home > div > div > div:nth-child(4) > home-healthy-habits > div > div > div:nth-child(4) > home-healthy-habit-tile > div > div.home-healthy-habit-yesno.ng-scope > button.btn-primary-inverse.vp-button-primary-inverse.yesNo-btn.yes-btn.ng-scope\')')
+    driver.execute_script('document.querySelector(\'#page-wrapper > div > div > div > basic-home > div > div > div:nth-child(4) > home-healthy-habits > div > div > div:nth-child(4) > home-healthy-habit-tile > div > div.home-healthy-habit-yesno.ng-scope > button.btn-primary-inverse.vp-button-primary-inverse.yesNo-btn.yes-btn.ng-scope\').click()')
     driver.quit()
+
 
 if __name__ == '__main__':
     virginpulsedailies()
